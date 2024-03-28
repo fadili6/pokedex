3e561b185293dcde8491e6ec660d45f331c63ed4
fadili6 <diallofadili@yahoo.fr>
2024-03-29 00:13:32 +0100

    gl

--- src/components/PockemonCard.jsx
+++ src/components/PockemonCard.jsx
@@ -1,12 +1,29 @@
-function PokemonCard() {
+import React from "react";
+
+const PokemonCard = () => {
+  // Récupération des données du premier Pokémon du tableau
+  const pokemon = pokemonList[0];
+
   return (
-    <div>
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-      </figure>
-      <figcaption>Balbasaur</figcaption>
+    <div className="pokemon-card">
+      <h2>{pokemon.name}</h2>
+      {/* Affichage de l'image du Pokémon s'il est disponible */}
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
     </div>
   );
-}
-
+};
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default PokemonCard;
